f1dbdd076bd5aaec1a172df41e6344571867cc0a
abdelaziz <d198550@yahoo.fr>
2024-10-09 08:51:03 +0200

    soustract positive amount great balance failed

--- src/test/java/test/operation/AccountOperation.java
+++ src/test/java/test/operation/AccountOperation.java
@@ -4,6 +4,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 
@@ -14,10 +15,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import exception.NonSufficientFundsException;
 import model.InMemoryTransactions;
 import model.Transaction;
 import service.Account;
-
 
 @ExtendWith(MockitoExtension.class)
 public class AccountOperation {
@@ -26,47 +27,59 @@ public class AccountOperation {
 
 	@Mock
 	private InMemoryTransactions transactions;
-	
+
 	@BeforeEach
 	void init() {
 		account = new Account(transactions);
 	}
-	
+
 	@Test
 	public void init_a_transaction_with_a_positive_amount_for_a_deposit() {
-		//Arrange
+		// Arrange
 		BigDecimal depositAmount = new BigDecimal(100L);
-		
-		//Act
+
+		// Act
 		account.deposit(depositAmount);
-		
-		//Assert
+
+		// Assert
 		verify(transactions, atLeastOnce()).add(any(Transaction.class));
 	}
-	
+
 	@Test
 	public void init_a_transaction_with_a_negative_amount_for_a_deposit() {
 		Assertions.assertThrows(IllegalArgumentException.class, () -> account.deposit(BigDecimal.valueOf(-100L)));
 	}
-	
+
 	@Test
 	public void init_a_transaction_with_a_null_amount_for_a_deposit() {
 		Assertions.assertThrows(IllegalArgumentException.class, () -> account.deposit(null));
 	}
-	
+
 	@Test
 	public void transaction_with_a_positive_amount_equals_to_the_balance_for_a_withdraw() {
-		
-	    // Arrange
-	    BigDecimal depositAmount = new BigDecimal("1000.00");
-	    BigDecimal withdrawalAmount = new BigDecimal("500.00");
-		
-	    
-	    //Act
+
+		// Arrange
+		BigDecimal depositAmount = new BigDecimal("1000.00");
+		BigDecimal withdrawalAmount = new BigDecimal("500.00");
+
+		// Act
 		account.deposit(depositAmount);
 		account.withdraw(withdrawalAmount);
-		
-		//Assert
+
+		// Assert
 		verify(transactions, times(2)).add(any(Transaction.class));
+	}
+
+	@Test
+	public void transaction_with_a_positive_amount_greater_than_the_balance_for_a_withdraw() {
+		// Arrange
+		BigDecimal depositAmount = new BigDecimal("500.00");
+		BigDecimal withdrawalAmount = new BigDecimal("1000.00");
+
+		// Act
+		account.deposit(depositAmount);
+
+		// Assert
+		Assertions.assertThrows(NonSufficientFundsException.class, () -> account.withdraw(withdrawalAmount));
 	}
 }
