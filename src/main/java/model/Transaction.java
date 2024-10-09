45a6492f9a5dab945ab3615c197d9f5c8ab2503d
abdelaziz <d198550@yahoo.fr>
2024-10-09 07:46:38 +0200

    init kata

---
+++ src/main/java/model/Transaction.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Transaction {
+	
+	public enum Type {
+		DEPOSIT,
+		WITHDRAW,
+	}
+	
+	private final LocalDateTime time;
+	private final BigDecimal amount;
+	private final BigDecimal balance;
+    private final Type type;
+    
+	public Transaction(Type type,LocalDateTime time, BigDecimal amount, BigDecimal balance) {
+		this.time = time;
+		this.amount = amount;
+		this.balance = balance;
+		this.type = type;
+	}
+
+	public LocalDateTime getTime() {
+		return time;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public Type getType() {
+		return type;
+	}
+}
