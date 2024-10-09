7bd50fddfd6e5d8cdb5271bf5c53d089bc69c9e8
abdelaziz <d198550@yahoo.fr>
2024-10-09 08:17:33 +0200

    add negative deposit failed

--- src/test/java/test/operation/AccountOperation.java
+++ src/test/java/test/operation/AccountOperation.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.verify;
 
 import java.math.BigDecimal;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -34,5 +35,10 @@ public class AccountOperation {
 	public void init_a_transaction_with_a_positive_amount_for_a_deposit() {
 		account.deposit(BigDecimal.valueOf(100L));
 		verify(transactions, atLeastOnce()).add(any(Transaction.class));
+	}
+	
+	@Test
+	public void init_a_transaction_with_a_negative_amount_for_a_deposit() {
+		Assertions.assertThrows(IllegalArgumentException.class, () -> account.deposit(BigDecimal.valueOf(-100L)));
 	}
 }
