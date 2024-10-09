b8cbe8d0f0db942a8e66b3ec313801e297fbae88
abdelaziz <d198550@yahoo.fr>
2024-10-09 08:19:26 +0200

    add negative deposit passed

--- src/main/java/service/Account.java
+++ src/main/java/service/Account.java
@@ -7,16 +7,22 @@ import model.InMemoryTransactions;
 import model.Transaction;
 
 public class Account {
-	
+
 	private InMemoryTransactions transactions;
-	
+
 	public Account(InMemoryTransactions transactions) {
 		this.transactions = transactions;
 	}
 
 	public void deposit(BigDecimal amount) {
+
+		if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+			throw new IllegalArgumentException("Cannot deposit a negative or null amount");
+		}
+
 		BigDecimal previousBalance = transactions.lastBalance();
-		Transaction transaction = new Transaction(Transaction.Type.DEPOSIT, LocalDateTime.now(), amount, previousBalance);
+		Transaction transaction = new Transaction(Transaction.Type.DEPOSIT, LocalDateTime.now(), amount,
+				previousBalance);
 		transactions.add(transaction);
 	}
 
